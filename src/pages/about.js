f5cb83226b413fb6fbe0d4097e008aa7b9eb1963
Fabio Coelho <fbclh@protonmail.com>
2022-03-06 19:25:54 -0300

    Finish part 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
+import Layout from '../components/layout';
 
-const About = () => {
+const AboutPage = () => {
   return (
-    <div>
-      <p>About me</p>
-    </div>
+    <Layout pageTitle="About Me">
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+    </Layout>
   );
 };
 
-export default About;
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
+import Layout from '../components/layout';
 
-const Index= () => {
+const IndexPage = () => {
   return (
-    <div>
-      <h1>Hello</h1> 
-      <p>I am learning Gatsby and React</p>
-    </div>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
   );
 };
 
-export default Index;
+export default IndexPage;
